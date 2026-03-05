a2cd8c0e8bca53452047e6bde0fee5f1c5d47777
Rob on Macbook Air <dutchrob@yahoo.com>
2026-03-04 21:39:12 -0800

    Initial Commit

---
+++ workout_aicodeTests/workout_aicodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  workout_aicodeTests.swift
+//  workout_aicodeTests
+//
+//  Created by Rob Boer on 3/4/26.
+//
+
+import Testing
+@testable import workout_aicode
+
+struct workout_aicodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
